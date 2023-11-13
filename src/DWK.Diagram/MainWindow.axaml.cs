b938aa061b9d384da315d3eb32ce2cac4b8feb3b
dwk-metabook <dingwk@live.com>
2023-11-13 16:01:11 +0800

    add project

---
+++ src/DWK.Diagram/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DWK.Diagram;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/DWK.Diagram/Program.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System;
+
+namespace DWK.Diagram;
+
+internal class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .With(new FontManagerOptions
+            {
+                DefaultFamilyName = "avares://DWK.Diagram/Assets/Fonts/SourceHanSansSC-Normal.otf#Source Han Sans SC",
+                FontFallbacks = new[]
+                {
+                    new FontFallback
+                    {
+                        FontFamily = "avares://DWK.Diagram/Assets/Fonts/SourceHanSansSC-Normal.otf#Source Han Sans SC"
+                    }
+                }
+            });
+}
